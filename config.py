531f39fb80123c8e0e70f85cb51984460764902c
petersudai <81692851+petersudai@users.noreply.github.com>
2024-06-12 15:13:24 +0300

    Re-add config.py with environment variables

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # SSL Configuration
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        'connect_args': {
+            'ssl': {
+                'ca': os.path.join(basedir, 'ca.pem')
+            }
+        }
+    }
